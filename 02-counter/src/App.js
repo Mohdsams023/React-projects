4d8f8bbcda0edbb4e291d1a5fe755473ce47913c
mohdsams23 <157204613+Mohdsams023@users.noreply.github.com>
2024-02-19 18:32:42 +0530

    added increse decrese project

--- 02-counter/src/App.js
+++ 02-counter/src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react';
 
-function App() {
-  
+const App = () => {
+    const [Counter, setCounter] = useState(0)
+    const Addvalue = () => {   
+        if (Counter < 20) {
+            setCounter(Counter + 1)
+        }   
+    };
+    const Removevalue = () => {
+        if (Counter > 0) {
+            setCounter(Counter - 1)
+        }   
+     }
   return (
-   <div className='bg-black text-white'>
-
-   </div>
-  );
+    <div className='flex items-center flex-col justify-center m-10'>
+      <h1>React Hooks</h1>
+      <h2>Counter Value : {Counter}</h2>
+      <button onClick={Addvalue} className='bg-black text-white p-2 m-3'>Add Value</button>
+      <button onClick={Removevalue} className='bg-black text-white p-2 m-3'>Decrease Value</button>
+    </div>
+  )
 }
-
-export default App;
+export default App
